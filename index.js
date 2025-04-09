ff5151cbb2fbd3439d047068dd8b551f233883f2
NaOH5 <kaya1678@outlook.com>
2025-04-09 22:22:26 +0300

    Update index.js

--- index.js
+++ index.js
@@ -38,7 +38,7 @@ app.listen(port, () => {
   console.log('\x1b[36m[ SERVER ]\x1b[0m', '\x1b[32m SH : http://localhost:' + port + ' ✅\x1b[0m');
 });
 
-const statusMessages = ["<decision:1359570482516459692> Serving Justice.", "🎮 Playing VALORANT"];
+const statusMessages = ["⚖️ Serving Justice.", "⚖️ Working on Appeals"];
 const statusTypes = [ 'dnd', 'idle'];
 let currentStatusIndex = 0;
 let currentTypeIndex = 0;
